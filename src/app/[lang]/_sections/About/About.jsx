b4aa9804c3455b44be639a7d06b60c31af09e238
pa2si <pascaliscoding@gmail.com>
2024-03-19 23:36:01 +0100

    mobile-bg-adjust

--- src/app/[lang]/_sections/About/About.jsx
+++ src/app/[lang]/_sections/About/About.jsx
@@ -9,12 +9,13 @@ const About = ({ fetchedData }) => {
       style={{
         backgroundImage: `url(${bg.filename})`,
         backgroundAttachment: 'fixed',
-        backgroundSize: '140%',
         backgroundPosition: 'center',
+        backgroundSize: 'cover',
       }}
+      className="mobile-bg-adjust"
     >
       <div
-        className="mobile-bg-adjust flex justify-center relative max-h-svh bg-gray-300 mx-4 bg-opacity-0  text-slate-50 overflow-auto sm:bg-opacity-40 xl:mx-0 rounded-lg"
+        className="flex justify-center relative max-h-svh bg-gray-300 mx-4 bg-opacity-0  text-slate-50 overflow-auto sm:bg-opacity-40 xl:mx-0 rounded-lg"
         id="about"
       >
         <div className="my-10 sm:mx-8 max-w-6xl xl:mx-auto pt-8 pb-1 lg:py-12 lg:px-16 bg-white bg-opacity-95 rounded-xl">
